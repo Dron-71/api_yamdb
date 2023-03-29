239892faf578277de1f054dde25df83d35135751
Тигран Саркисян <s.tigo@yandex.ru>
2023-03-29 17:44:03 +0500

    Добавил модели ещё модели

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,6 +5,28 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(
+        unique=True,
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Review(models.Model):
     text = models.TextField()
     author = models.ForeignKey(
@@ -22,6 +44,22 @@ class Review(models.Model):
 
     def __str__(self):
         return self.text[:15]
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=150)
+    year = models.DateTimeField()
+    description = models.TextField(blank=True)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='titles'
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Comment(models.Model):

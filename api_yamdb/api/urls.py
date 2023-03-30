a393eb29e3731ee601ba34221163dd8a52289e37
Тигран Саркисян <s.tigo@yandex.ru>
2023-03-30 15:36:12 +0500

    cheanged some models

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views
 
-from api_yamdb.api.views import (
+from api.views import (
     ReviewViewSet, CommentViewSet)
 
 router = DefaultRouter()
@@ -30,7 +30,6 @@ router.register(
 
 urlpatterns = [
     path(r'v1/', include(router.urls)),
-    path(r'v1/', include('djoser.urls')),  # Работа с пользователями
 ]
 
 urlpatterns += [
